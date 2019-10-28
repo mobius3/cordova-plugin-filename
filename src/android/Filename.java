e79ceae82398384e34b81dbe5113a866cca01081
Leonardo <leonardo.guilherme@gmail.com>
2019-10-28 10:57:59 -0300

    use method from docs to get file name

--- src/android/Filename.java
+++ src/android/Filename.java
@@ -3,6 +3,7 @@ package mobius3.plugin;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.support.v4.content.CursorLoader;
 
 import org.apache.cordova.*;
@@ -15,28 +16,23 @@ public class Filename extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("getFilename")) {
             String uri = args.getString(0);
-            this.getRealPathFromURI(uri, callbackContext);
+            this.getFileNameAndSendToContext(uri, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void getRealPathFromURI(String uri, CallbackContext callbackContext) {
-        if (uri != null && uri.length() > 0) {
-            callbackContext.success(this.getFile(uri));
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }
-
-    private String getFile(String uri) {
+    private void getFileNameAndSendToContext(String uri, CallbackContext callbackContext) {
+      if (uri != null && uri.length() > 0) {
         Uri contentUri = Uri.parse(uri);
-        String[] proj = { MediaStore.Images.Media.DATA };
-        CursorLoader loader = new CursorLoader(cordova.getActivity(), contentUri, proj, null, null, null);
-        Cursor cursor = loader.loadInBackground();
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        return cursor.getString(column_index);
+        Cursor returnCursor = cordova.getContext().getContentResolver().query(contentUri, null, null, null, null);
+        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        returnCursor.moveToFirst();
+        callbackContext.success(returnCursor.getString(nameIndex));
+      } else {
+        callbackContext.error("Expected one non-empty string argument.");
+      }
     }
 }
+
 

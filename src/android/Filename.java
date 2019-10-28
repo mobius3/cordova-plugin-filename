88eac8ebdb4e793a48a5a8dc9522cb3e32b3ad13
Leonardo <leonardo.guilherme@gmail.com>
2019-10-28 09:24:12 -0300

    add filename getting code

--- src/android/Filename.java
+++ src/android/Filename.java
@@ -7,15 +7,31 @@ import org.json.JSONException;
 public class Filename extends CordovaPlugin {
 
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("getRealFilename")) {
-            String url = data.getString(0);
-            String message = "Filename, " + url;
-            callbackContext.success(message);
-
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("getFileName")) {
+            String uri = args.getString(0);
+            this.getRealPathFromURI(uri, callbackContext);
             return true;
-        } else {
-            return false;
         }
+        return false;
+    }
+
+    private void getRealPathFromURI(String uri, CallbackContext callbackContext) {
+        if (uri != null && uri.length() > 0) {
+            callbackContext.success(this.getFile(uri));
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private String getFile(String uri) {
+        Uri contentUri = Uri.parse(uri);
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(cordova.getActivity(), contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String fileName = cursor.getString(column_index);
+        return fileName;
     }
 }
